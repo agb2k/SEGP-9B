7fe176a12cce9a9c72c416ff319fe77d0e959e25
Kong Soon Ing <happy_sooning@hotmail.com>
2020-11-12 22:15:21 +0800

    expandable record

--- app/src/main/java/medPal/App/BloodPressureActivity.java
+++ app/src/main/java/medPal/App/BloodPressureActivity.java
@@ -6,13 +6,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ExpandableListView;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class BloodPressureActivity extends AppCompatActivity {
     private Button b3;
+    ExpandableListView expandableListView;
+    List<String> listGroup;
+    HashMap<String,List<String>> listItem;
+    MainAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +29,14 @@ public class BloodPressureActivity extends AppCompatActivity {
         setContentView(R.layout.activity_blood_pressure);
 
         getSupportActionBar().setTitle("Blood Pressure");
+
+        expandableListView = findViewById(R.id.expandablelistview);
+        listGroup = new ArrayList<>();
+        listItem = new HashMap<>();
+        adapter = new MainAdapter(this, listGroup, listItem);
+        expandableListView.setAdapter(adapter);
+        initListData();
+        
 
         b3 = (Button) findViewById(R.id.NewPressureRecordButton);
         b3.setOnClickListener(new View.OnClickListener() {
@@ -44,6 +61,23 @@ public class BloodPressureActivity extends AppCompatActivity {
 
 
     }
+
+    private void initListData() {
+        listGroup.add(getString(R.string.november));
+
+        String[] array;
+
+        List<String> list = new ArrayList<>();
+        array = getResources().getStringArray(R.array.november);
+        for (String item : array){
+            list.add(item);
+        }
+
+        listItem.put(listGroup.get(0),list);
+        adapter.notifyDataSetChanged();
+
+    }
+
     public void openNewPressureRecord() {
         Intent intent1 = new Intent(this, NewPressureRecord.class);
         startActivity(intent1);
