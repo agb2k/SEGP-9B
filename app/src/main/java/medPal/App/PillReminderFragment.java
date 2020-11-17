2ef21eead40b11aefc35614e5088b35d07312428
sayft1 <sayft1@nottingham.edu.my>
2020-11-17 13:47:14 +0800

    change some icon

--- app/src/main/java/medPal/App/PillReminderFragment.java
+++ app/src/main/java/medPal/App/PillReminderFragment.java
@@ -59,6 +59,9 @@ public class PillReminderFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pill_reminder, container, false);
+        View v = inflater.inflate(R.layout.fragment_pill_reminder, container, false);
+
+
+        return v;
     }
 }
