ed53200df59730a193a791240db2697302dc07ac
Abhinav George Basil <abhinav.basil@gmail.com>
2021-04-22 23:43:08 +0800

    Change default email

--- app/src/main/java/medPal/App/UserIdentification/UserIdentity.java
+++ app/src/main/java/medPal/App/UserIdentification/UserIdentity.java
@@ -29,7 +29,7 @@ public class UserIdentity {
         this.email = email;
     }
 
-    public String getEmail() { return email==null?"abhinav.basil@gmail.com":email; }
+    public String getEmail() { return email==null?"no_identity":email; }
 
     public boolean loggedIn() {
         return loggedIn;
